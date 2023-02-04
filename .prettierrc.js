d0cf7373bd4266b5c3a722f2afcf3bbf426bc069
Dustin Do <dustin.do95@gmail.com>
2023-02-04 20:36:51 +0700

    Initialize remix

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
