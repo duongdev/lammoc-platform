d990ba5530b3ccb09b2de207e99fb8e03e688e75
Dustin Do <dustin.do95@gmail.com>
2023-02-12 02:33:25 +0700

    Update vercel config

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ['**/.*'],
   future: {
     v2_routeConvention: true,
