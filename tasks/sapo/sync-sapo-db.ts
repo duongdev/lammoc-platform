5a15b5f586ee6808b26fcf9a7681233302d05aae
Dustin Do <dustin.do95@gmail.com>
2023-03-02 00:26:12 +0700

    Update sync script

--- tasks/sapo/sync-sapo-db.ts
+++ tasks/sapo/sync-sapo-db.ts
@@ -9,17 +9,17 @@ export const sync = async (tenant: SapoTenant) => {
   log('Sync sapo data...')
   const sapo = new Sapo(tenant)
 
-  // const { account } = await sapo.profiles()
-  // log(`Signed in as [${account.id}] ${account.full_name}`)
+  const { account } = await sapo.profiles()
+  log(`Signed in as [${account.id}] ${account.full_name}`)
 
-  // log(`Sync customers`)
-  // await sapo.syncCustomers()
+  log(`Sync customers`)
+  await sapo.syncCustomers()
 
-  // log('Sync product categories')
-  // await sapo.syncProductCategories()
+  log('Sync product categories')
+  await sapo.syncProductCategories()
 
-  // log('Sync products')
-  // await sapo.syncProducts()
+  log('Sync products')
+  await sapo.syncProducts()
 
   log('Sync orders')
   await sapo.syncOrders()
