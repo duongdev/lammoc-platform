36a8a35c106b5772132545cc80111342e13314c6
Dustin Do <dustin.do95@gmail.com>
2023-03-02 02:27:12 +0700

    Update remix.config

--- remix.config.js
+++ remix.config.js
@@ -1,17 +1,21 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
+
 module.exports = {
-  // serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: process.env.VERCEL ? 'vercel' : undefined,
+  server:
+    process.env.NODE_ENV === 'development' || !process.env.VERCEL
+      ? undefined
+      : './server.js',
   ignoredRouteFiles: ['**/.*'],
   future: {
     v2_routeConvention: true,
     v2_meta: true,
-    v2_errorBoundary: false
+    v2_errorBoundary: true,
   },
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  publicPath: "/build/",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 }
