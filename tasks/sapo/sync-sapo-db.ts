82f7cc1f7d7fe0afb682ba5536c29be96b688b45
Dustin Do <dustin.do95@gmail.com>
2023-03-04 16:33:10 +0700

    Update sync script

--- tasks/sapo/sync-sapo-db.ts
+++ tasks/sapo/sync-sapo-db.ts
@@ -3,9 +3,11 @@ import Debug from 'debug'
 import { Sapo } from './sapo.service'
 import type { SapoTenant } from './sapo.type'
 
-const log = Debug('Sapo:sync-sapo')
+const _log = Debug('Sapo:sync-sapo')
 
-const sync = async (tenant: SapoTenant) => {
+const syncAll = async (tenant: SapoTenant) => {
+  const log = _log.extend(`syncAll:${tenant}`)
+
   log('Sync sapo data...')
   const sapo = new Sapo(tenant)
 
@@ -21,13 +23,27 @@ const sync = async (tenant: SapoTenant) => {
   log('Sync products')
   await sapo.syncProducts()
 
+  // log('Sync orders')
+  // await sapo.syncOrders()
+}
+
+const syncNewOrders = async (tenant: SapoTenant): Promise<void> => {
+  const log = _log.extend(`syncAll:${tenant}`)
+
+  log('Sync new orders...')
+  const sapo = new Sapo(tenant)
+
+  const { account } = await sapo.profiles()
+  log(`Signed in as [${account.id}] ${account.full_name}`)
+
   log('Sync orders')
-  await sapo.syncOrders()
+  await sapo.syncOrders({ onlyModified: true })
+
+  syncNewOrders(tenant)
 }
 
-const syncAll = async (): Promise<void> => {
-  await Promise.all([sync('thichtulam'), sync('store-lam-moc')])
-  return syncAll()
-}
+syncNewOrders('store-lam-moc')
+syncNewOrders('thichtulam')
 
-syncAll()
+syncAll('store-lam-moc')
+syncAll('thichtulam')
