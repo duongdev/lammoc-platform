554a9ef4f415fbc89bd5fc0ab42f04a0f486dcd4
Dustin Do <dustin.do95@gmail.com>
2023-06-10 15:10:55 +0700

    Remove appwrite from dependencies

--- app/libs/appwrite.server.ts
+++
@@ -1,8 +0,0 @@
-import Server, { Users } from 'node-appwrite'
-
-export const awServer = new Server.Client()
-  .setEndpoint(process.env.APPWRITE_ENDPOINT ?? '')
-  .setProject(process.env.APPWRITE_PROJECT ?? '')
-  .setKey(process.env.APPWRITE_API_KEY ?? '')
-
-export const awUsers = new Users(awServer)
--- app/libs/appwrite.ts
+++
@@ -1,7 +0,0 @@
-import { Account, Client } from 'appwrite'
-
-export const awClient = new Client()
-  .setEndpoint('https://brains.withDustin.com/v1')
-  .setProject('tlm-platform')
-
-export const awAccount = new Account(awClient)
